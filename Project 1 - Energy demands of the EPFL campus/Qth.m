6511c7df44a5fd90c290d52fad86ed38331121b7
deifilia <deifilia.to@gmail.com>
2020-02-26 16:11:20 +0100

    Add helper functions to solve heat equation

--- Project 1 - Energy demands of the EPFL campus/Qth.m
+++ Project 1 - Energy demands of the EPFL campus/Qth.m
@@ -1,4 +1,5 @@
-
-function q = Qth(deltaT, Ath, kth, T_int, T_ext, k_sun, ith, q, f_el, Q_el)
-    q = deltaT*(Ath*(kth*(T_int-T_ext)-k_sun*ith-q)-f_el*Q_el)
+function q = Qth(deltaT, Ath, kth, T_int, T_ext, k_sun, ith, Q_gain, f_el, Q_el, index)
+    % TODO: Check Q_gain, remove index here.
+    index = 1;
+    q = deltaT*(Ath*(kth*(T_int-T_ext(index))-k_sun*ith(index)-Q_gain(index))-f_el*Q_el);
 end
---
+++ Project 1 - Energy demands of the EPFL campus/q_plus_year.m
@@ -0,0 +1,12 @@
+function q_year = q_plus_year (deltaT, Ath, kth, T_int, T_ext, k_sun, ith, q, f_el, Q_el)
+    q_year = 0;
+    for (i = 1:length(T_ext))
+        Q_val = Qth(deltaT, Ath, kth, T_int, T_ext, k_sun, ith, q, f_el, Q_el, i);
+        if (Q_val >= 0)
+            q_ts = Q_val;
+        else
+            q_ts = 0;
+        end
+        q_year = q_year + q_ts;
+    end 
+end
