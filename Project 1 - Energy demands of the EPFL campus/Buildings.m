1df748e205a54260f58bfd346951f13f9e954a57
mjj4617 <56396942+mjj4617@users.noreply.github.com>
2020-02-26 16:13:23 +0100

    Update Buildings.m

--- Project 1 - Energy demands of the EPFL campus/Buildings.m
+++ Project 1 - Energy demands of the EPFL campus/Buildings.m
@@ -65,15 +65,20 @@ OccProf_Cantine = [0 0 0 0 0 0 0 0 0.4 0.2 0.4 1 0.4 0.2 0.4 0 0 0 0 0 0 0 0 0];
 OccProf_Class = [0 0 0 0 0 0 0 0.4 0.6 1 1 0.8 0.2 0.6 1 0.8 0.8 0.4 0 0 0 0 0 0];% Each hour from 1 am to 24 pm
 
 %% TASK 2 - Calculation of the building thermal properties (kth and ksun)
-m_air  = 2.5 % m3/m2h
 
+% First equation - switching ON the heating system
 function q = Qth(deltaT, Build.ground, k_th, T_int, Text, k_sun, Irr, q, f_el, Q_el)
     q = delta_t*(Build.ground*(k_th*(T_int-Text)-k_sun*Irr-q)-f_el*Q_el)
 end
 
-% First equation - switching ON the heating system
-
+    
 % Second equation - yearly heating demand
+if Text<16
+    
+end
+if Text>26
+    Q_th = 0
+end 
 
 % Implementation of the Newton-Raphson method
     
