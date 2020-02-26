09511852a9868a9104f7705fcb62e8a51edb3596
SimonVerstraeten <s.verstraeten98@gmail.com>
2020-02-26 15:54:23 +0100

    Update Buildings.m
    Standard code modifications

--- Project 1 - Energy demands of the EPFL campus/Buildings.m
+++ Project 1 - Energy demands of the EPFL campus/Buildings.m
@@ -67,13 +67,13 @@ p.electric.week.v = p.electric.week.f * Q_el;
 p.electric.year.v = p.electric.year.f * Q_el;
 
 % 1.2 - Presence of people (all the buildings are the same)
-Heat_gain = [5, 35, 23.3, 0]; %[W/m^2]
+Heat_gain = [5, 35, 23.3, 0];       %[W/m^2]
 Share     = [0.3, 0.05, 0.35, 0.3]; %[-]
 
 % Occupation profile for office, restaurant and classroom from 1 am to 24 pm
 OccProf_Office = [0 0 0 0 0 0 0 0.2 0.4 0.6 0.8 0.8 0.4 0.6 0.8 0.8 0.4 0.2 0 0 0 0 0 0]; %[-]
-OccProf_Rest   = [0 0 0 0 0 0 0 0 0.4 0.2 0.4 1 0.4 0.2 0.4 0 0 0 0 0 0 0 0 0]; %[-]
-OccProf_Class  = [0 0 0 0 0 0 0 0.4 0.6 1 1 0.8 0.2 0.6 1 0.8 0.8 0.4 0 0 0 0 0 0]; %[-]
+OccProf_Rest   = [0 0 0 0 0 0 0 0 0.4 0.2 0.4 1 0.4 0.2 0.4 0 0 0 0 0 0 0 0 0];           %[-]
+OccProf_Class  = [0 0 0 0 0 0 0 0.4 0.6 1 1 0.8 0.2 0.6 1 0.8 0.8 0.4 0 0 0 0 0 0];       %[-]
 
 % Matrix of occupation profile for office, restaurant, classroom and other
 OccProf = [OccProf_Office; OccProf_Rest; OccProf_Class; zeros(1,24)]; %[-]
