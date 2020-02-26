642566672a2335bca8f832b3e2092a0375f39853
deifilia <deifilia.to@gmail.com>
2020-02-26 10:51:54 +0100

    initial files

---
+++ Project 1 - Energy demands of the EPFL campus/All_Buildings.m
@@ -0,0 +1,44 @@
+function All_Buildings
+
+
+%% PREAMBLE
+% Course: Modelling and Optimisation of Energy Systems
+% Project: Energy demands of the EPFL campus
+% Authors: Francesca Belfiore
+% Last modified: 18/2-2020
+%
+% Description: Global Matlab function to RUN to calculate, 
+% for all buildings:
+% (1) the internal heat gains 
+% (2) the building envelope properties (Newton-Raphson)
+% (3) the hourly heating demand
+% (4) the typical periods (clustering)
+% based on the Buildings.m file
+%% 
+
+%% Initialisation
+clear all
+close all
+clc
+
+%% Data import (Buildings.csv)
+filename = 'P1_buildingsdata.csv';
+fid = fopen(filename);
+format = '%s%f%f%f%f%f';
+data = textscan(fid,format,'Headerlines',1,'delimiter',',');
+name = data{1,1};
+n_build = length(name);
+
+%% Function call (Buildings.m)
+% The following can be deleted or uncommented depending on the organisation
+% of your Buildings.m file.
+
+% for i = 1:n_build
+%     building_name = name{i,1};
+%     Build = Buildings(building_name);
+%     format2 = '%s%s%f%s%f\n';
+%     
+%     fprintf(fid,format2,building_name,',',Build.kth,',',Build.ksun);
+%     fclose(fid);
+%     
+% end
---
+++ Project 1 - Energy demands of the EPFL campus/Buildings.m
@@ -0,0 +1,70 @@
+function Build = Buildings(building_name)
+
+%% PREAMBLE
+% Course: Modelling and Optimisation of Energy Systems
+% Project: Energy demands of the EPFL campus
+% Authors: Francesca Belfiore
+% Last modified: 18/2-2020
+%
+% Description: function to CALL to calculate, for A GIVEN building:
+% (1) the internal heat gains 
+% (2) the building envelope properties (Newton-Raphson)
+% (3) the hourly heating demand
+% (4) the typical periods (clustering)
+%% 
+
+%% DATA MANAGEMENT
+%  Do NOT modify
+
+% Initial parameters
+h = 8760;                   % Number of hours in a year
+T_th = 16;                  % Cut-off temperature of the heating system [�C]
+cp_air = 1152;              % Specific heat capacity of the air [J/m3/K] 
+T_int = 21;                 % Set point (comfort) temperature [�C]
+air_new = 2.5;              % Air renewal [m3/m2]
+Vent = air_new*cp_air/3600; % Ventilation capacity
+
+% Call of the weather data
+filename = 'P1_weatherdata.csv';
+data_w = csvread(filename,1,0);
+Text = data_w(:,1);         % External temperature [�C]
+Irr = data_w(:,2);          % Global solar irradiation [W/m2]
+
+% Call of the buildings data
+filename = 'P1_buildingsdata.csv';
+fid = fopen(filename);
+format = '%s%f%f%f%f%f'
+data = textscan(fid, format, 'Headerlines', 1, 'delimiter', ',');
+name = data{1,1};
+
+% Index and variable definition
+index = find(ismember(name, building_name));
+Build.ground = data{1,3}(index);    % Building heated surface [m2]
+Build.env = data{1,4}(index);       % Building thermal envelope [m2]
+Build.Q = data{1,5}(index);         % Building annual heat load [kWh]
+Build.El = data{1,6}(index);        % Building annual electricity consumption [kWh]
+
+%% TASK 1 - Calculation of the internal heat gains (appliances & humans)
+
+% 1.1 - Electronic appliances and lights for each buildings 
+
+% 1.2 - Presence of people (all the buildings are the same)
+
+%% TASK 2 - Calculation of the building thermal properties (kth and ksun)
+
+% First equation - switching ON the heating system
+
+% Second equation - yearly heating demand
+
+% Implementation of the Newton-Raphson method
+    
+    % Method initialisation
+    
+    % Resolution
+
+%% TASK 3 - Estimation of the hourly profile    
+
+% Hourly demand (thermal load)
+
+%% TASK 4 - Clustering of the heating demand
+% based on the hourly heating demand (typical periods)
