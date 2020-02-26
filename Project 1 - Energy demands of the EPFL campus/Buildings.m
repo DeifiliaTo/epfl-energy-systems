301a24cbe122013a4ea3a23c5bf67cb28b9c3e8f
mjj4617 <56396942+mjj4617@users.noreply.github.com>
2020-02-26 14:00:25 +0100

    Update Buildings.m

--- Project 1 - Energy demands of the EPFL campus/Buildings.m
+++ Project 1 - Energy demands of the EPFL campus/Buildings.m
@@ -46,9 +46,13 @@ Build.El = data{1,5}(index);        % Building annual electricity consumption [k
 %% TASK 1 - Calculation of the internal heat gains (appliances & humans)
 
 % 1.1 - Electronic appliances and lights for each buildings 
+Day_ele = [0 0 0 0 0 0 1 1 1 1 1 1 1 1 1 1 1 1 1 1 0 0 0 0];
+Week_ele = [Day_ele;Day_ele;Day_ele;Day_ele;Day_ele;zeros(2,24)];
+Year_ele = 
 Operating_hours = 3654 %[h/year]
 f_el = 0.8
-Q_el = Build.El*f_el*Operating_hours %[kJ]
+Q_el = Build.El*f_el/Operating_hours %[kJ]
+Ele_profile = Q_el*
 
 % 1.2 - Presence of people (all the buildings are the same)
 heat_gain = [5, 35, 23.3, 0] %[W/m^2]
