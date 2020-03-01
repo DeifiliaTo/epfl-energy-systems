7c8f77ddd973abe8559942d13d0deba3463f856e
SimonVerstraeten <s.verstraeten98@gmail.com>
2020-03-01 10:45:12 +0100

    Update Clustering.m
    Clustering by kmeans method

--- Project 1 - Energy demands of the EPFL campus/Clustering.m
+++ Project 1 - Energy demands of the EPFL campus/Clustering.m
@@ -28,24 +28,38 @@ Irr_norm = (Irr - Irr_min)/(Irr_max - Irr_min);
 
 %% Sorting Text_norm and Irr_norm into Weather_norm
 
-% Weather_norm is a matrix of 365 columns and 48 lines
-% The 24 first lines (1 to 24) contain the hourly temperature of the 365
-% days
-% The 24 last lines (25 to 48) contain the hourly irradiance of the 365
-% days
+% Weather_norm is a matrix of 365 lines and 48 columns
+% The 24 first columns (1 to 24) contain the normalized hourly temperature
+% of the 365 days
+% The 24 last columns (25 to 48) contain the normalized hourly irradiance
+% of the 365 days
 
-Weather_norm = zeros (48,365);
+Weather_norm = zeros (365,48);
 
 % Extracting temperature and irradiance at each hour throughout the year
 for i = 1:24
     for j = 1:365
-        Weather_norm (i,j) = Text_norm(i + (j-1)*24);
-        Weather_norm ((24 + i),j) = Irr_norm(i + (j-1)*24);
+        Weather_norm (j,i) = Text_norm(i + (j-1)*24);
+        Weather_norm (j,(24 + i)) = Irr_norm(i + (j-1)*24);
     end
 end
 
 
-%% kmeans clustering
+%% kmeans clustering and indicators
+
+% Clustering into 12 typical days
+% idx contains the index for the 365 days, refering to one of the 12
+% typical days
+% C contains the 24 Text_norm and Irr_norm of the 12 typical days
+[idx, C] = kmeans (Weather_norm, 12);
+
+% Profile deviation for each typical period
+% C(idx(i),:)
+
+% Profile deviation for the entire year
+
+% Maximum load duration curve difference
+
 
 
 
