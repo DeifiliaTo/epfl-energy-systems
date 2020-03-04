179d2cde869842585ba3de4eefd2e376aa751794
Andreas Richardson <andreas.richardson@epfl.ch>
2020-03-04 16:36:39 +0100

    rationalise Q functions

--- Project 1 - Energy demands of the EPFL campus/Qth.m
+++ Project 1 - Energy demands of the EPFL campus/Qth.m
@@ -1,12 +1,12 @@
-function q = Qth(deltaT, Ath, kth, T_int, T_ext, k_sun, ith, q_people, Q_el, heat_switch)
-% Qth calculate heat load (eq. 1.3)
+function Q = Qth(deltaT, Ath, kth, T_int, T_ext, k_sun, ith, q_people, Q_el, heat_switch)
+%Qth calculate heat load (eq. 1.3)
 % Calculation multiplies the time interval by the energy required, and
 % energy provided by irradiation, electrical, and by people
 
     % Only perform calculation if the exterior temperature is < 16 degrees
     % and we're in the heating periods
     if T_ext < 16 && heat_switch == 1
-        q = deltaT * ( Ath * ( kth * ( T_int-T_ext ) - k_sun * ith - q_people ) - Q_el);
+        Q = deltaT * ( Ath * ( kth * ( T_int-T_ext ) - k_sun * ith - q_people ) - Q_el);
     else
-        q = 0;
+        Q = 0;
 end
--- Project 1 - Energy demands of the EPFL campus/q_plus_year.m
+++ Project 1 - Energy demands of the EPFL campus/q_plus_year.m
@@ -1,16 +1,16 @@
+function q_year = q_plus_year (deltaT, Ath, kth, T_int, T_ext, k_sun, ith, q_people, Q_el_year, heat_switch)
 % Calculation of Q+ over all the time intervals
 % Eqn (1.4):
 % If Q_th >= 0, add Qth(t) to sum. Otherwise, add 0
 
-function q_year = q_plus_year (deltaT, Ath, kth, T_int, T_ext, k_sun, ith, q_people, Q_el_year, heat_switch)
-    q_year = 0;
-    for (i = 1:length(T_ext))
-        Q_val = Qth(deltaT, Ath, kth, T_int, T_ext(i), k_sun, ith(i), q_people(i), Q_el_year(i), heat_switch(i));
-        if (Q_val >= 0)
-            q_ts = Q_val;
-        else
-            q_ts = 0;
-        end
-        q_year = q_year + q_ts;
-    end 
+    % template matrix to use for constants
+    z = ones(length(T_ext),1);
+    
+    % calculate heat load and demand for each time step
+    heat_load   = arrayfun(@Qth, deltaT*z, Ath*z, kth*z, T_int*z, T_ext, k_sun*z, ith, q_people, Q_el_year, heat_switch);
+    heat_demand = arrayfun(@Qth_plus, heat_load);
+    
+    % sum the demand over the whole year
+    q_year = sum(heat_demand);
+    
 end
