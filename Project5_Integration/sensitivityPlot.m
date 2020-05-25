52040809d7e0267f0f7afd87bdc486e8d332722c
Andreas Richardson <andreas.richardson@epfl.ch>
2020-05-25 16:38:41 +0100

    update sensitivity plots

--- Project5_Integration/sensitivityPlot.m
+++ Project5_Integration/sensitivityPlot.m
@@ -1,6 +1,16 @@
 function d = sensitivityPlot(file,dims)
-%SENSITIVITYPLOT Summary of this function goes here
-%   Detailed explanation goes here
+%SENSITIVITYPLOT Takes a data file and generates a sensitivity plot
+%   Data file is read using readtable. Columns / variables are interpreted
+%   as follows
+%       Col     Usage
+%       1       category string (name of variable being changed)
+%       2       multiplication factor of dependent variable (plot: X2)
+%       3       adjusted value of dependent variable (plot: X1)
+%       4       dependent variable value
+%   If dims ([width height] in cm) is supplied, figures are exported.
+%
+%   Example usage: sensitivityPlot('sensitivity_TC.dat');
+
 
 % load(file)
 d = readtable(file);
@@ -22,8 +32,9 @@ for j = 1:numel(uniqueVars)
     
     % format
     ax1 = gca;
+    ax1.Box = 'off';
     
-    ax1.XTick = d_{:,3};
+    % ax1.XTick = d_{:,3};
     ax1.XAxis.Label.String = var;
     ax1.XAxis.Label.Interpreter = 'none';
     
@@ -37,14 +48,14 @@ for j = 1:numel(uniqueVars)
         'YAxisLocation','right',...
         'Color','none');
     
-    % X2: sync ticks & lims; label differently
-    ax2.XTick = ax1.XTick;
-    ax2.XTickLabel = d_{:,2};
+    % X2: add ticks for alpha
+    ax2.XTick = d_{1:2:end,3};
+    ax2.XTickLabel = d_{1:2:end,2};
     ax2.XLim = ax1.XLim;
     % ax2.XAxis.Label.String = d.Properties.VariableNames{2};
     % ax2.XAxis.Label.Interpreter = 'none';
     
-    % Y2: just sync lims
+    % Y2: sync lims
     ax2.YLim = ax1.YLim;
     ax2.YTick = {};
     
