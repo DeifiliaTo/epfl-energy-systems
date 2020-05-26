03b352a819fe91b5897a3cd8e9bd6d5c89ac521f
mjj4617 <56396942+mjj4617@users.noreply.github.com>
2020-05-26 12:41:49 +0200

    to merge

--- Project5_Integration/figExport.m
+++ Project5_Integration/figExport.m
@@ -5,4 +5,5 @@ fig.PaperSize = [w h];
 fig.PaperPosition = [0 0 w h];
 % fig.Renderer = 'Painters'; % for 3D plots
 print(gcf, '-dpdf', [pwd '/figures/' name '.pdf']);
+savefig(gcf, [pwd '/figures/' name '.fig'])
 end
